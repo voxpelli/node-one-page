e37489683a1f344e5f6af469bb7f2be7fa5eb90a
Pelle Wessman <pelle@kodfabrik.se>
2015-12-08 21:00:06 +0100

    feat(main): easier rendering of new pages

---
+++ test/integration/extendability.spec.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const request = require('supertest-as-promised');
+
+describe('Node One Page – Extended', function () {
+  const VTOnePage = require('../../');
+  const dbUtils = require('../db-setup');
+
+  let config;
+  let AppClass;
+  let appInstance;
+  let app;
+
+  const createApp = function (protoProps, staticProps) {
+    AppClass = VTOnePage.extend(protoProps, staticProps);
+    config = AppClass.ExpressConfig.getConfig({});
+    appInstance = new AppClass(config);
+    app = appInstance.getApp();
+
+    return dbUtils.setup(appInstance);
+  };
+
+  afterEach(function () {
+    if (appInstance) {
+      return appInstance.close();
+    }
+  });
+
+  describe('path', function () {
+    beforeEach(function () {
+      let protoProps = {};
+
+      protoProps.getRoutes = function () {
+        let routes = VTOnePage.prototype.getRoutes.call(this);
+
+        routes['/foo'] = (req, res, next) => {
+          this
+            .render(res)
+            .then(result => res.send(result))
+            .catch(err => next(err));
+        };
+
+        return routes;
+      };
+
+      return createApp(protoProps);
+    });
+
+    it('should return a web page on extended path', function () {
+      return request(app)
+        .get('/foo')
+        .expect(200)
+        .expect('Content-Type', 'text/html; charset=utf-8');
+    });
+  });
+});
